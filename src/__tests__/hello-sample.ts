876fcde62cd71ef6e7669b3e3d11fa17ae04d95d
7korobi <7korobi@gmail.com>
2021-03-19 16:21:01 +0900

    init yarn test

---
+++ src/__tests__/hello-sample.ts
@@ -0,0 +1,17 @@
+import "../lib/test-env.js"
+import { render } from "@testing-library/svelte"
+
+/*
+import App from "../App.svelte"
+
+test("should render", () => {
+  const results = render(App, { props: { name: "world" } })
+
+  expect(() => results.getByText("Hello world!")).not.toThrow()
+  expect(results).toMatchSnapshot()
+})
+*/
+
+test("should render", () => {
+  expect(1).toEqual(1)
+})
