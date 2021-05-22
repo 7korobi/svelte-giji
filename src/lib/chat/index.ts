8f0ca8c69cf8b2c729fe0b0db54d433b2b468514
7korobi <7korobi@gmail.com>
2021-05-23 04:36:07 +0900

    chat index.

---
+++ src/lib/chat/index.ts
@@ -0,0 +1,9 @@
+import Banner from './Banner.svelte'
+import Talk from './Talk.svelte'
+import Post from './Post.svelte'
+import Report from './Report.svelte'
+
+import Chat from './Chat.svelte'
+
+export default Chat
+export { Talk, Post, Report, Banner }
