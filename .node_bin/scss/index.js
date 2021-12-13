b0d3b283d3bb6bc2d752639007d2dae7548fe430
7korobi <7korobi@gmail.com>
2021-12-13 23:04:20 +0900

    .node_bin include

---
+++ .node_bin/scss/index.js
@@ -0,0 +1,19 @@
+import sass from "sass";
+import glob from "glob";
+import fs from "fs";
+import functions, { save } from "./functions.js";
+glob("./src/scss/*.sass", (err, files) => {
+  files.forEach((file) => {
+    const outFile = file.replace("./src/scss/", "./static/css/").replace(".sass", ".css");
+    console.log(`${outFile} <== ${file}`);
+    const { css } = sass.renderSync({
+      file,
+      outFile,
+      functions,
+      outputStyle: "compressed",
+      sourceMap: false
+    });
+    fs.writeFileSync(outFile, css);
+  });
+  save();
+});
