2a36a3548de8f2ebdac6d9e4cd654d3deb849fee
7korobi <7korobi@gmail.com>
2021-12-27 03:43:02 +0900

    index.ts not mix .svelte & .ts

--- src/lib/design/index.ts
+++ src/lib/design/index.ts
@@ -5,9 +5,4 @@ import Sup from './sup.svelte'
 import Sub from './sub.svelte'
 import SearchText from './search-text.svelte'
 import LongPress from './long-press.svelte'
-
-// export * from './util'
-// export * from './bits'
-// export * from './bits-n'
-
 export { Btn, Grid, Strong, Sup, Sub, SearchText, LongPress }
